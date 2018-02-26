d1f01ebe76f30d766b2310b830dffa4bc655380c
zwetsloot robert jan <zwetsloot.robert.jan@techcross.co.jp>
2018-02-26 15:09:52 +0900

    fixed issues where adding descriptions to classes would ignore the class definition

--- test/block_parser_test.exs
+++ test/block_parser_test.exs
@@ -10,13 +10,13 @@ defmodule BlockParserTest do
           "class GlobalClass { // global class comment\n  -List<int> _cache // caches stuff\n  +int GetCache(int index) // gets cache\n}"
         ],
         "namespace UI.Scripts.PageViewModel.TestEvent {\n  \n\n  \n\n  \n}" => [
-          "interface ITestHeaderViewModel {\n    +string TestImageResourceID\n  }",
-          "class TestHeaderViewModel {\n    +string TestImageResourceID\n  }",
-          "class TestEventViewModel {\n    +string Id\n    -int _count\n    +void OnClick(int buttonType, string id)\n    -int GetCount()\n  }"
+          "interface ITestHeaderViewModel { // comment 4\n    +string TestImageResourceID\n  }",
+          "class TestHeaderViewModel { // comment 3\n    +string TestImageResourceID\n  }",
+          "class TestEventViewModel { // comment 2\n    +string Id\n    -int _count\n    +void OnClick(int buttonType, string id)\n    -int GetCount()\n  }"
         ],
         "namespace UI.Scripts.PageView.TestEvent {\n  \n\n  \n}" => [
-          "enum ButtonType {\n    Ok\n    Cancel\n  }",
-          "class TestEventView {\n    +Animator animation\n    +void Bind(TestEventViewModel viewModel)\n  }"
+          "enum ButtonType { // comment 6\n    Ok\n    Cancel\n  }",
+          "class TestEventView { // comment 5\n    +Animator animation\n    +void Bind(TestEventViewModel viewModel)\n  }"
         ]
       }
     end).()
